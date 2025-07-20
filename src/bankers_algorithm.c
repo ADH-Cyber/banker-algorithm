aaacfc5c46b699db1b0172d43cb079a1f0ed48f6
Austin Howard <austin.d.howard@proton.me>
2025-07-20 17:08:05 -0400

    Parse available[] from command-line arguments

--- src/bankers_algorithm.c
+++ src/bankers_algorithm.c
@@ -18,6 +18,10 @@
  *  gcc -o banker bankers_algorithm.c
 */
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
 #define NUMBER_OF_CUSTOMERS 5
 #define NUMBER_OF_RESOURCES 4
 
@@ -33,6 +37,33 @@ int allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 // The remaining need of each customer
 int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 
-// Function prototypes
+/*
+ * FUNCTION PROTOTYPES
+*/ 
 int request_resources(int customer_num, int request[]);
 void release_resources(int customer_num, int release[]);
+
+
+int main(int argc, char *argv[]) {
+    if (argc != NUMBER_OF_RESOURCES + 1) {
+        printf(
+            "Usage: %s <resource1> <resource2> ... <resource%d>\n", 
+            argv[0], 
+            NUMBER_OF_RESOURCES
+        );
+        return 1;
+    }
+
+    // Parse available[] from command-line
+    for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
+        available[i] = atoi(argv[i + 1]);
+    }
+
+    // Debug print to verify values
+    printf("Initial available resources:\n");
+    for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
+        printf("Resource %d: %d\n", i, available[i]);
+    }
+
+    return 0;
+}
