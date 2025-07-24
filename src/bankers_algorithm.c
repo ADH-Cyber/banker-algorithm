9715aa511b2a76fd39b9613af93d03d81f752695
Austin Howard <austin.d.howard@proton.me>
2025-07-23 23:02:18 -0400

    File Block comment

--- src/bankers_algorithm.c
+++ src/bankers_algorithm.c
@@ -1,3 +1,23 @@
+/*
+ * File: bankers_algorithm.c
+ * Author: Austin Howard
+ * Date: 2025-07-27
+ * 
+ * Description:
+ *  Simulates the Banker’s Algorithm for deadlock avoidance by managing multiple customer 
+ *  requests and releases for shared resources. The program checks for safe states using 
+ *  the Safety Algorithm and ensures allocations do not lead to unsafe states that could 
+ *  cause deadlock.
+ * 
+ * Course: 
+ *  CS7343 - Operating Systems
+ *  Dr. Maya El Dayeh
+ *  Southern Methodist University
+ * 
+ * Compile using:
+ *  gcc -o banker src/bankers_algorithm.c
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
